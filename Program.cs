929054d154227fa0f5780e98b54f4bf74497e782
Michell Cronberg <michell@cronberg.dk>
2018-03-01 00:43:48 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            // Keep console window open when using the debugger (F5)
+            if (System.Diagnostics.Debugger.IsAttached) {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
